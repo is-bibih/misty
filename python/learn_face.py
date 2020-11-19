99ed5d8769c027506513e4786eb827c5ee7c7251
vivi <vivianamaldes@gmail.com>
2020-11-19 15:49:28 -0600

    add python script to learn a face

---
+++ python/learn_face.py
@@ -0,0 +1,26 @@
+from mistyPy import Robot
+import time
+
+# IP address
+IP = "192.168.1.69"
+# person's name
+name = "vivi"
+
+# create robot instance
+misty = Robot(IP)
+
+# check battery
+#print("battery: " + misty.battery())
+
+# learn a face
+print("about to start face learning...")
+time.sleep(5)
+misty.learnFace(name)
+
+# check if face was learned
+if name in misty.getLearnedFaces():
+    print(name + "'s face learned successfully")
+else:
+    print("face not learned")
+print("learned faces:\n" + misty.getLearnedFaces())
+
