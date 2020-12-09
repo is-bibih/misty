ec38f52232b7cd4023b18ef1024546fa4e47818c
vivi <vivianamaldes@gmail.com>
2020-12-09 15:52:09 -0600

    adds functions for tts and greeting

--- python/speech.py
+++ python/speech.py
@@ -1,24 +1,53 @@
+import os
+import time
 import gtts
 from mistyPy import Robot
+
+# if known: greet (wave + speech + image)
+# if unknown: learn
+
+def misty_tts(robot, message, lang='en', fname='temp.mp3'):
+    # generate file
+    sound = gtts.gTTS(message, lang=lang)
+    sound.save(fname)
+    # upload and play
+    robot.uploadAudio(fname, overwrite=True)
+    robot.playAudio(fname)
+    os.remove(fname)
+
+def learn_face(robot):
+    misty_tts(robot,
+              'Por favor ve a la computadora.',
+              lang='es')
+    name = input('Introduce tu nombre.\n')
+    misty_tts(robot,
+              'Por favor pon tu cara 30 centímetros'
+              + 'en frente de mí',
+              lang='es')
+    time.sleep(7)
+    misty.learnFace(name)
+    # check whether it was learned
+    if name in robot.getLearnedFaces():
+        message = 'He aprendido tu cara.'
+    else:
+        message = 'No pude aprender tu cara.'
+    misty_tts(robot, message, lang='es')
+    time.sleep(5)
+
+def greet(robot, name, image='e_Admiration.jpg'):
+    robot.changeImage(image)
+    # arm, position, speed percent 
+    robot.moveArmDegrees('right', -89, 30)
+    misty_tts(robot, 'hola, ' + name, lang='es')
+    time.sleep(2)
+    robot.moveArmDegrees('right', 50, 30)
+    robot.changeImage('e_DefaultContent.jpg')
 
 # IP address
 IP = "192.168.1.94"
 
-# language for text to speech
-lang = "es"
-
-# file to save to
-fname = "sound_for_misty.mp3"
-
 # initialize misty robot object
 misty = Robot(IP)
 
-# create speech audio file
-message = "hola"
-sound = gtts.gTTS(message, lang=lang)
-sound.save(fname)
-
-# upload and play
-misty.uploadAudio(fname, overwrite=True)
-misty.playAudio(fname)
+learn_face(misty)
 
