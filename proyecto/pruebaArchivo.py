ad90261aef81c0e55cef3a9ad93a8f34900adc38
Luis <a01283738@itesm.mx>
2021-06-28 10:05:51 -0500

    Se uso archivo para prueba, si senor

---
+++ proyecto/pruebaArchivo.py
@@ -0,0 +1,14 @@
+"""abocado"""
+def agua(i=5):
+	"""agua
+
+	Parameters
+	----------
+	i: int, default 5
+		Pee pee pooooo descripcion No. 2
+
+	Notes
+	-----
+	Esto es un mensaje para todos los autobots
+	"""
+	pass
